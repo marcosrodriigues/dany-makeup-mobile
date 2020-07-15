925edb56ca1b95c08ea0537d76619a307df9f798
marcosrodriigues <marcos.rodriiigues@gmail.com>
2020-07-14 21:44:47 -0300

    teste

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'react-native'
 import { Provider } from 'react-redux' 
-
+import './shim.js'
 // import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
 // import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
 
